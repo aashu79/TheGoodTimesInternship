b597206441b06e6b27be0efd7a0e8d051e6be07a
aashu79 <aashusah435@gmail.com>
2024-03-10 18:01:54 +0545

    add link to logo name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,33 @@
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="w-full p-8 bg-gray-800 flex justify-between">
+      <Link
+        to={"/"}
+        style={{ textDecoration: "none", color: "white" }}
+        className="cursor-pointer"
+      >
         <h1 className="text-4xl font-bold text-white">Beer Gardern</h1>
-        <div className="flex gap-6">
-            <Link to={"/"} style={{textDecoration: "none", color: "white"}} className="cursor-pointer">
-            <h5 className="hover:text-red-500">Home</h5>
-            </Link>
-            <Link to={"/search"} style={{textDecoration: "none", color: "white"}} className="cursor-pointer">
-            <h5 className="hover:text-red-500">Search</h5>
-            </Link>
-        </div>
+      </Link>
+      <div className="flex gap-6">
+        <Link
+          to={"/"}
+          style={{ textDecoration: "none", color: "white" }}
+          className="cursor-pointer"
+        >
+          <h5 className="hover:text-red-500">Home</h5>
+        </Link>
+        <Link
+          to={"/search"}
+          style={{ textDecoration: "none", color: "white" }}
+          className="cursor-pointer"
+        >
+          <h5 className="hover:text-red-500">Search</h5>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
